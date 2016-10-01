6430bfb266df735ab3a164e35d7c780fb9756d27
c2huc2hu <c2huc2hu@gmail.com>
2016-10-01 12:32:40 -0400

    working server with basic api

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('hello, world!')
+var express = require('express'); 
+var app = express(); 
+
+var port = process.env.PORT || 8080;
+var model = {led: 100, pot: 456}; 
+
+var ledRouter = express.Router();
+ledRouter.get('/value', (request, response) => {
+    console.log('response is', {value: model.led}); 
+    response.json({value: model.led}); 
+}); 
+ledRouter.put('/value/:val', (request, response) => {
+    console.log('made post request', request.params.val)
+    model.led = request.params.val; 
+    response.json({value: model.led}); 
+}); 
+app.use('/led', ledRouter); 
+
+var potRouter = express.Router(); 
+potRouter.get('/value', (request, response) => { 
+    console.log('response is', {value: model.pot}); 
+    response.json({value: model.pot}); 
+}); 
+app.use('/pot', potRouter); 
+
+app.listen(port)
+console.log('started on port', port)
