f4765751f9493b0702b1060c9677edcbdef127ef
c2huc2hu <c2huc2hu@gmail.com>
2016-10-22 18:03:36 -0400

    Promisified serial, fixed a bug that prevented serial from actually being used

--- server/main.js
+++ server/main.js
@@ -1,13 +1,13 @@
-// command line arguments to control which systems are enabled
-var program = require('commander')
+var program = require('commander') // command line arguments to control which systems are enabled
  .usage('node main.js [options]')
  .option('-s, --serial', 'Enable arduinos connected over serial')
+ .parse(process.argv)
  // add logging options
-
 
 var api = require('./basic_server');
 var system = program.serial ? require('./serial_connection') : require('./dummy_system');
 
+console.log('Started server, loading modules ...');
 model = {led: 500, pot: 300};
 api.init(model);
 system.init(model);
--- server/serial_connection.js
+++ server/serial_connection.js
@@ -1,16 +1,22 @@
 var SerialPort = require("serialport");
-var port = new SerialPort("COM4", {
-	baudRate: 9600
-});
+var Promise = require('bluebird');
+
+var getSerialPorts = Promise.promisify(SerialPort.list);
 
 init = function(model) {
-	port.on('data', function(data) {
-		potValue = /sensor = (\d+)/.exec(data.toString())[1]; 
-		model.pot = potValue; 
-		
-		console.log('<', data.toString(), '| led:', model.led, '>');
-		port.write(model.led.toString());
-	});
+	getSerialPorts()
+	.then(portInfo => {
+		var port = new SerialPort(portInfo[0].comName);
+		console.log('-> started serial port');
+		port.on('data', function(data) {
+			potValue = /sensor = (\d+)/.exec(data.toString())[1];
+			model.pot = potValue;
+
+			console.log('<', data.toString(), '| led:', model.led, '>');
+			port.write(model.led.toString());
+		});
+	})
 }
 
-module.exports = {init}; 
+
+module.exports = {init};
