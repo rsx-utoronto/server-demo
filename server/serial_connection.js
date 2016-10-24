cff66da9ef9cca7484ccae4c1bbf13239d0c22dc
c2huc2hu <c2huc2hu@gmail.com>
2016-10-24 12:58:39 -0400

    bug fix to serial parser. the raw parser works fine if you have a delay, removed .ds_stores

--- server/serial_connection.js
+++ server/serial_connection.js
@@ -6,12 +6,13 @@ var getSerialPorts = Promise.promisify(SerialPort.list);
 init = function(model, config) {
 	getSerialPorts()
 	.then(portInfo => {
-		var port = new SerialPort(portInfo[0].comName, 
-		{ 
-			parser: SerialPort.parsers.byteLength(6);
-		}); 
+		var port = new SerialPort(portInfo[0].comName,
+		{
+			parser: SerialPort.parsers.byteLength(6)
+		});
 		console.log('-> started serial port on ', portInfo[0].comName);
 		port.on('data', function(data) {
+			console.log(data.toString())
 			potValue = /sensor = (\d+)/.exec(data.toString())[1];
 			model.pot = potValue;
 
