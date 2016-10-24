c45001346b47ab9b0f91ce0572c63363e778c9ee
Richard Seo <richardseo@Richards-MacBook-Pro.local>
2016-10-24 17:26:28 -0400

    Motor driver

--- arduino_thermistor/Thermistor/Thermistor.ino
+++ arduino_thermistor/Thermistor/Thermistor.ino
@@ -1,15 +1,30 @@
-int photocellPin = 0; 
-void setup() { 
-  Serial.begin(9600); 
+int photocellPin = 0;
+void setup() {
+  Serial.begin(9600);
+
 }
 
-void loop() { 
-delay(1000); 
-float Vout = analogRead(photocellPin); 
-Serial.println(Vout);
-//float Vout = 5.0; //simulating 
-float Temp = (Vout/1024.0)*5000; //convert to millivolts
-float cel = Temp/10; //get temperature in celcius (1Cel/10mv ratio)
-Serial.println(cel);
-delay(1000);
+void loop() {
+  delay(1000);
+  float Vout = analogRead(photocellPin);
+  //Serial.println(Vout);
+  //float Vout = 5.0; //simulating
+  float Temp = (Vout / 1024.0) * 5000; //convert to millivolts
+  float cel = Temp / 10; //get temperature in celcius (1Cel/10mv ratio)
+
+  //Serial.print("Temperature "),cel," 11"," 12"," 13"," 14"," 15");
+  
+  Serial.print("Temperatures ");
+  for (int i = 0; i<7; i++){
+    Serial.print(cel+i);
+    Serial.print(" ");
+    }
+  Serial.println(" ");
+  Serial.print("Currents " );
+  for (int j = 0; j<7; j++){
+    Serial.print(10+j);
+    Serial.print(" ");
+    }
+  Serial.println(" ");
+  delay(1000);
 }
